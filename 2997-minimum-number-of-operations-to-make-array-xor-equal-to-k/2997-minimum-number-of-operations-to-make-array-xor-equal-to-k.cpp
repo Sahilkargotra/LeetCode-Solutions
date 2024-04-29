9e3fbb3dfee6919d08e6c691a2be5986b1278b0a
Sahil Kargotra <133209822+Sahilkargotra@users.noreply.github.com>
2024-04-29 20:31:15 +0530

    Time: 124 ms (29.67%), Space: 91.4 MB (54.80%) - LeetHub

---
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int ans = 0;
+        for(int i =0 ;i<=20;i++)
+        {
+            int xorVal = 0;
+            for(auto x: nums)
+            {
+                int bitVal = (1<<i)&x;
+                xorVal = xorVal^bitVal;
+            }
+            int kBitVal = (1<<i) &k;
+            if(xorVal != kBitVal) ans++;
+        
+        }
+        return ans;
+    }
+};
